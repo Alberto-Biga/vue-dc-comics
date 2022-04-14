467e1643b60c7faca7d95870252073b3b7d940ae
AlbertoBiga <alberto.biga10@gmail.com>
2022-04-14 16:13:52 +0200

    nav-bar

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">    
+    <NavComp/>
+  </div>
+</template>
+
+<script>
+import NavComp from './components/NavComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;  
+}
+</style>
---
+++ src/components/NavComp.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="bg-white">
+    <div class="nav-container">
+    <!-- LEFT NAV -->
+    <div class="nav-img">
+      <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <!-- RIGHT NAV -->
+    <div>
+      <ul>
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
+    </div>    
+  </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'NavComp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+  .bg-white{
+    background-color: white;
+  }
+  .nav-container{
+    display: flex;    
+    justify-content: space-between;
+    padding: 20px;
+    width:80%;
+    margin: 0 auto;
+    line-height: 50px;
+    /*TO DELETE*/
+    background-color: aqua;
+  }
+  /*LIST CHANGES */
+  ul{
+    list-style-type: none;
+    display: flex;
+    // text-align: center;
+    flex-direction: row;
+
+      li{
+        margin: 0 15px;
+        padding: 5px;
+        font-size: 15px;
+      }
+  } 
+  .nav-img{
+    display: flex;    
+  }
+</style>
